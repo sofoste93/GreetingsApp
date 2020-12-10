be0a7c0325651a4a127bec21cb321f0dfa4b03d8
Kreativfunction <stefan-kruger7@hotmail.com>
2020-12-10 23:09:23 +0100

    Add files via upload
    I created this file separately (main())
    I found it nice and elegant as a way to organize files while writing code.

---
+++ Main.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package greetings;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+/**
+ * @author sofoste
+ * GreetingsApp
+ */
+@SuppressWarnings("unused")
+public class Main extends GreetingsApp {
+
+	/**
+	 * Launch the application.
+	 * run the app
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//GreetingsApp g = new GreetingsApp();
+		//g.frame = new JFrame();
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GreetingsApp window = new GreetingsApp();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
